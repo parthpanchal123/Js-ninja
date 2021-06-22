25d9cde24874ed7fa9ab059500d3aea7756437c2
Parth Panchal <parthpanchal53@gmail.com>
2021-06-22 17:39:27 +0530

    Added trick #4

---
+++ skip_elements_array_destructuring/index.js
@@ -0,0 +1,6 @@
+const Fruits = ["Apple", "Banana", "Chickoo", "Pineapple"];
+
+const [, , ...restFruits] = Fruits; // This will skip the first two elements as seperated by the , delimiter
+
+console.log(restFruits);
+s;
