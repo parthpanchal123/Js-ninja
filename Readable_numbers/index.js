819bf51665923d5e958a5f3860761c2beed7bee6
Parth Panchal <parthpanchal53@gmail.com>
2021-06-24 09:37:11 +0530

    Added trick #12

---
+++ Readable_numbers/index.js
@@ -0,0 +1,5 @@
+const largeNo = 1000000000000; // Dont do this since it's not readable
+
+const largeNo = 1000_000_000_000; // This is much better in terms of readability as compared to previous way
+
+const largeNo = 1e12; // You also dont need to type all zeros
